bba6b5872c806cf9a64ec0d087df3e6078171f5a
borox345 <borox.tv@gmail.com>
2022-01-07 15:21:45 +0100

    font update

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,13 @@
 import Head from 'next/head';
 import { FaDiscord } from 'react-icons/fa';
 import { FaGithub } from 'react-icons/fa';
+import { SiTypescript } from 'react-icons/si'
 
 export default function Home() {
 
 
   return (
-    <div className="container flex items-center p-4 mx-auto min-h-screen justify-center">
+    <div className="container flex items-center justify-center p-4 mx-auto min-h-screen">
       <Head>
         <title>borox.me</title>
         <meta property="og:description" content="cool guy."></meta>
@@ -16,22 +17,33 @@ export default function Home() {
         <meta name="keywords" content="borox"></meta>
         <meta property="og:locale" content="en-US"></meta>
       </Head>
-      
-      <main>
-        <h1 className="lg:text-6xl text-4xl font-bold  text-white text-center hover:scale-110 transition-transform">
-          Hi! I'm <span className="text-[#5865F2] font-extrabold cursor-pointer">borox</span>!
-        </h1>
-        <p className='text-white text-center lg:text-2xl md:text-xl text-xl'>
-          <span className='font-bold'>front-end</span> begginer and <span className='font-bold'>python</span> enjoyer
-        </p>
 
-        <div className='flex items-center justify-center relative top-[20px]'>
-          <a href='https://discord.com/users/314424536256872449' target='_blank' className='bg-[#3d48c0] opacity-80 hover:opacity-100 duration-500 w-[3rem] h-[3rem] rounded-lg flex items-center justify-center hover:scale-110 transition-transform'>
-            <FaDiscord className='fill-[#b9bfff] w-[30px] h-[30px] cursor-pointer'/>
-          </a>
-          <a href='https://github.com/borox345' target='_blank' className='bg-[#1a1a1a] opacity-90 hover:opacity-100 duration-500 w-[3rem] h-[3rem] rounded-lg flex items-center justify-center relative left-5 hover:scale-110 transition-transform'>
-            <FaGithub className='fill-[#979595] w-[30px] h-[30px] cursor-pointer'/>
-          </a>
+      <main>
+        <div className=''>
+
+          <h1 className="lg:text-6xl text-4xl font-black text-white text-center hover:scale-110 transition-transform">
+            Hi! I'm <span className="text-[#5865F2] cursor-pointer">borox</span>!
+          </h1>
+          <p className='text-white text-center lg:text-2xl md:text-xl text-xl'>
+            <span className='font-bold'>front-end</span> begginer and <span className='font-bold'>python</span> enjoyer
+          </p>
+
+          <div className='flex items-center justify-center relative top-[20px]'>
+            <a href='https://discord.com/users/314424536256872449' target='_blank' className='bg-[#3d48c0] opacity-80 hover:opacity-100 duration-500 w-[3rem] h-[3rem] rounded-lg flex items-center justify-center hover:scale-110 transition-transform'>
+              <FaDiscord className='fill-[#b9bfff] w-[1.875rem] h-[1.875rem] cursor-pointer' />
+            </a>
+            <a href='https://github.com/borox345' target='_blank' className='bg-[#1a1a1a] opacity-90 hover:opacity-100 duration-500 w-[3rem] h-[3rem] rounded-lg flex items-center justify-center relative left-5 hover:scale-110 transition-transform'>
+              <FaGithub className='fill-[#979595] w-[1.875rem] h-[1.875rem] cursor-pointer' />
+            </a>
+          </div>
+
+        </div>
+
+
+
+        <div className='relative top-[32rem] text-left' id='left-content'>
+          <h1 className='text-white text-4xl'>technologies</h1>
+          <SiTypescript className='fill-[#358EF1] h-[1.875rem] w-[1.875rem] ' />
         </div>
 
       </main>
