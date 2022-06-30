0786c6467734f5a9de93da7a20945c0a793710bd
borox345 <borox.tv@gmail.com>
2022-06-30 11:54:02 +0200

    update: new contact frame

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,14 @@
 import Head from 'next/head';
+import Image from 'next/image'
 import React, { useState, useEffect } from 'react'
 import axios from "axios";
 
 import { BsTwitter } from 'react-icons/bs'
 import { BsDiscord } from 'react-icons/bs'
 import { BiMailSend } from 'react-icons/bi'
-
 import { IoIosArrowDown } from 'react-icons/io'
+
+import memoji  from '../../assets/images/meMoji.png'
 
 export default function Home() {
 
@@ -16,10 +18,10 @@ export default function Home() {
         <title>borox</title>
       </Head>
 
-      <section id='home' className='min-h-full px-4 flex flex-col justify-center items-center text-center lg:mt-[350px] md:mt-[300px] mt-[250px]'>
+      <section id='home' className='min-h-full px-4 flex flex-col justify-center items-center text-center lg:mt-[300px] py-8'>
         <div className='flex items-center justify-center'>
-          <img className='rounded-full w-[128px] h-[128px] object-center flex  hover:animate-spin' src='https://i.imgur.com/PZsvhHZ.png'></img>
-        </div>
+          <Image height={'150px'} width={'150px'} className='rounded-full object-center flex  hover:animate-spin' src={memoji}/>
+        </div> 
 
         <h2 className="lg:text-6xl md:text-6xl text-5xl font-black text-center mt-10">
           Hey, I'm borox!
@@ -29,11 +31,40 @@ export default function Home() {
           <span className='hover:font-black text-[#3c86be]'><a target='_blank' href='https://python.org'>python</a> </span> enjoyer
         </p>
 
-        {/* <div className='flex items-center justify-center animate-pulse flex-grow mt-10'>
+        <div className='flex items-center justify-center flex-grow mt-20 animate-bounce'>
           <a href='#contact'>
             <IoIosArrowDown className='w-16 h-16 ' />
           </a>
-        </div> */}
+        </div>
+      </section>
+
+      <section id='contact' className='py-16 px-8 flex flex-col justify-center items-center min-h-screen'>
+        <h1 className='text-6xl font-black text-center'>Contact!</h1>
+
+        <div className='text-xl grid gap-3 mt-5 text-center font-semibold contact-grid'>
+          <a href='https://twitter.com/borox345' className='h-[1.5em] w-[1.5em] text-[#1DA1F2] justify-self-end cursor-pointer'>
+            <BsTwitter className='w-8 h-8'/>
+          </a>
+          <a href='https://twitter.com/borox345'>@borox345</a>
+
+          <a href='https://discord.com/users/314424536256872449' className='justify-self-end h-[1.5em] w-[1.5em] text-[#5865f2] cursor-pointer'>
+            <BsDiscord className='w-8 h-8'/>
+          </a>
+          <div>
+            <a href='https://discord.com/users/314424536256872449'>
+              borox
+              <span className='text-[#858383] hover:text-[#e6e4e4]'>#0178</span>
+            </a>
+          </div>
+
+          {/* <a href='mailto:borox.tv@gmail.com' className='justify-self-end h-[1.5em] w-[1.5em] text-[#ff4e4e] cursor-pointer'>
+            <BiMailSend className='w-8 h-8'/>
+          </a>
+          <a href='https://twitter.com/borox345'>borox@borox.me</a> */}
+          
+
+        </div>
+
       </section>
 
 
