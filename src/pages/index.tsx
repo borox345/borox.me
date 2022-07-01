06a074c4d906c3fec2864a028e2014a573a13bec
borox345 <borox.tv@gmail.com>
2022-07-01 12:11:29 +0200

    new: github link

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,6 +6,8 @@ import axios from "axios";
 import { BsTwitter } from 'react-icons/bs'
 import { BsDiscord } from 'react-icons/bs'
 import { BiMailSend } from 'react-icons/bi'
+import { BsGithub } from 'react-icons/bs'
+import { FaPython } from 'react-icons/fa'
 import { IoIosArrowDown } from 'react-icons/io'
 
 import memoji  from '../../assets/images/meMoji.png'
@@ -28,7 +30,7 @@ export default function Home() {
         </h2>
 
         <p className='text-lg leading-6 tracking-wide mb-12 pt-4 text-center font-black'>
-          <span className='hover:font-black text-[#3c86be]'><a target='_blank' href='https://python.org'>python</a> </span> enjoyer
+          <FaPython className='inline-flex w-6 h-6'/> <span className='hover:font-black text-[#3c86be]'><a target='_blank' href='https://python.org'>python</a> </span> enjoyer <br/>
         </p>
 
         <div className='flex items-center justify-center flex-grow mt-20 animate-bounce'>
@@ -39,13 +41,18 @@ export default function Home() {
       </section>
 
       <section id='contact' className='py-16 px-8 flex flex-col justify-center items-center min-h-screen'>
-        <h1 className='text-6xl font-black text-center'>Contact!</h1>
+        <h1 className='text-6xl font-black text-center'>Socials!</h1>
 
         <div className='text-xl grid gap-3 mt-5 text-center font-semibold contact-grid'>
           <a href='https://twitter.com/borox345' className='h-[1.5em] w-[1.5em] text-[#1DA1F2] justify-self-end cursor-pointer'>
             <BsTwitter className='w-8 h-8'/>
           </a>
           <a href='https://twitter.com/borox345'>@borox345</a>
+
+          <a href='https://github.com/borox345' className='h-[1.5em] w-[1.5em] text-[#4e4d4e] justify-self-end cursor-pointer'>
+            <BsGithub className='w-8 h-8'/>
+          </a>
+          <a href='https://github.com/borox345'>@borox345</a>
 
           <a href='https://discord.com/users/314424536256872449' className='justify-self-end h-[1.5em] w-[1.5em] text-[#5865f2] cursor-pointer'>
             <BsDiscord className='w-8 h-8'/>
